9a12d64e0276c07044ecb54ec41ac96b942d7170
“Sreyas-03” <futuresreyas@gmail.com>
2022-04-19 15:26:30 +0530

    printing output on terminal, printing only words with frequency > 1, other minor changes on file structures

--- q3-b.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-file=quotes.txt
-if (($# >= 1))
-then
-    file=$1
-fi
-
-echo number of lines = $(wc -l $file | awk '{print $1}') # number of lines in the file
--- q3.sh
+++ q3.sh
@@ -15,8 +15,10 @@ echo number of lines = $(wc -l $file | awk '{print $1}') # number of lines in th
 echo number of words = $(wc -w $file | awk '{print $1}') # number of words in the file
 
 awk '{print "Line No:",((1+i++)),"-",NF}' $file > wordsPerLine.txt # number of words on each line
+cat wordsPerLine.txt
 
 tr "[[:space:]&&~]" "\n" < $file| sed 's/[[:punct:]]$//' | sort -f | uniq -ic > temp.txt
 sed '/[A-Za-z]/!d' temp.txt > temp1.txt
-awk '{print "Word:",$2,"-",$1}' temp1.txt > wordFreq.txt
+awk '{if($1 >= 2) print "Word:",$2,"-",$1}' temp1.txt > wordsFreq.txt
+cat wordsFreq.txt
 rm temp.txt temp1.txt
